b9b46aa5bb5838afee65d2d2a1ec0b522560c5a7
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-11-22 20:27:59 +0700

    change datepicker

--- src/partials/actions/Datepicker.jsx
+++ src/partials/actions/Datepicker.jsx
@@ -21,9 +21,9 @@ function Datepicker() {
 
   return (
     <div className="relative">
-      <Flatpickr className="form-input pl-9 text-slate-500 hover:text-slate-600 font-medium focus:border-slate-300 w-60" options={options} />
+      <Flatpickr className="form-input pl-9 bg-companyL dark:bg-companyD text-companyL-400 dark:text-companyD-400 hover:bg-companyL-200 dark:hover:bg-companyD-200 shadow-lg shadow-gray-400/30 font-medium focus:border-companyL-400 dark:focus:border-companyD-400 w-60" options={options} />
       <div className="absolute inset-0 right-auto flex items-center pointer-events-none">
-        <svg className="w-4 h-4 fill-current text-slate-500 ml-3" viewBox="0 0 16 16">
+        <svg className="w-4 h-4 fill-current text-companyL-400 dark:text-companyD-400 ml-3" viewBox="0 0 16 16">
           <path d="M15 2h-2V0h-2v2H9V0H7v2H5V0H3v2H1a1 1 0 00-1 1v12a1 1 0 001 1h14a1 1 0 001-1V3a1 1 0 00-1-1zm-1 12H2V6h12v8z" />
         </svg>
       </div>

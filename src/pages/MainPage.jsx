a1713b636f714d7ed50407f1a9f5328b5c0c4204
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-11-22 19:42:13 +0700

    fix filter button

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -34,8 +34,8 @@ function MainPage() {
                     {/* Datepicker built with flatpickr */}
                     <Datepicker/>
                     {/* Add view button */}
-                    <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white">
-                        <svg className="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                    <button className="btn bg-companyL dark:bg-companyD text-companyL-400 dark:text-companyD-400 hover:bg-companyL-200 dark:hover:bg-companyD-200 shadow-lg shadow-gray-400/30">
+                        <svg className="w-4 h-4 fill-companyL-400 opacity-80 shrink-0" viewBox="0 0 16 16">
                             <path
                                 d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z"/>
                         </svg>

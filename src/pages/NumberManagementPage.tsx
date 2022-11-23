3a1bea883141b1a3057f8e59c2fc26d033bf39d2
Sergey <50526865+Kosrotoff@users.noreply.github.com>
2022-11-24 01:01:21 +0700

    Added logic in number management page

--- src/pages/NumberManagementPage.tsx
+++ src/pages/NumberManagementPage.tsx
@@ -12,7 +12,6 @@ export default function NumberManagementPage() {
     const [subscriptionPrice, setSubscriptionPrice] = useState(BigNumber.from(0));
     const [myNumbers, setMyNumbers] = useState([] as boolean[]);
     const [codeInfo, setCodeInfo] = useState<any>(null);
-    const [codeMode, setCodeMode] = useState(0);
 
     const {account, ENSName} = useWeb3React();
 
@@ -26,9 +25,6 @@ export default function NumberManagementPage() {
                     rootRouter?.subscriptionPrice()
                         .then(setSubscriptionPrice)
                         .catch(console.error)
-                    rootRouter?.getMode(BigNumber.from(firstNum))
-                        .then(setCodeMode)
-                        .catch(console.error)
                     rootRouter?.pool(BigNumber.from(firstNum))
                         .then(setCodeInfo)
                         .then(() => setIsLoadedodeInfo(true))
@@ -41,9 +37,6 @@ export default function NumberManagementPage() {
 
     const selectNumber = (num: string | number) => {
         setIsLoadedodeInfo(false);
-        rootRouter?.getMode(BigNumber.from(num))
-            .then(setCodeMode)
-            .catch(console.error)
         rootRouter?.pool(BigNumber.from(num))
             .then(setCodeInfo)
             .then(() => setIsLoadedodeInfo(true))
@@ -58,6 +51,158 @@ export default function NumberManagementPage() {
             .then(() => selectNumber(selectedCode))
             .catch(() => selectNumber(selectedCode))
     }
+
+    function InputAddress({placeholder, state, setState}: {placeholder: string, state: string, setState: any}) {
+        return (
+            <input
+                className='ml-2 p-1 bg-companyL'
+                type="text"
+                placeholder={placeholder}
+                value={state}
+                onChange={(event) => {
+                    const result = event.target.value.replace(/[^0-9a-zA-Z]+/g, '');
+                    setState(result);
+                }}
+            />
+        )
+    }
+
+    function InputString({placeholder, state, setState}: {placeholder: string, state: string, setState: any}) {
+        return (
+            <input
+                className='ml-2 p-1 bg-companyL'
+                type="text"
+                placeholder={placeholder}
+                value={state}
+                onChange={(event) => {
+                    setState(event.target.value);
+                }}
+            />
+        )
+    }
+
+    function InputNumber({placeholder, state, setState}: {placeholder: string, state: string, setState: any}) {
+        return (
+            <input
+                className='ml-2 p-1 bg-companyL'
+                type="text"
+                placeholder={placeholder}
+                value={state}
+                onChange={(event) => {
+                    const result = event.target.value.replace(/\D/, '');
+                    setState(result);
+                }}
+            />
+        )
+    }
+
+    function NumberSettings() {
+        const [newOwner, setNewOwner] = useState('');
+        const [newSipDomain, setNewSipDomain] = useState('');
+
+        return (
+            <>
+                <div>
+                    <p>Info:</p>
+                    <p>mode: {codeInfo.mode == 0 ? 'Number' : 'Pool'}</p>
+                    <p>sipDomain: {codeInfo.hasSipDomain ? codeInfo.sipDomain : '<DEFAULT>'}</p>
+                </div>
+                <div className='pt-10'>
+                    <p>Methods:</p>
+                    <p>
+                        <button className="border-1 rounded-lg w-[200px] bg-companyL p-1 ml-0 m-2" onClick={() => {
+                            rootRouter?.changeCustomerNumberMode(BigNumber.from(selectedCode))
+                                .catch(console.error);
+                        }}>changeMode</button>
+                    </p>
+                    <p>
+                        <button className="border-1 rounded-lg w-[200px] bg-companyL p-1 ml-0 m-2" onClick={() => {
+                            rootRouter?.transferOwnershipOfCustomerNumber(BigNumber.from(selectedCode), newOwner)
+                                .catch(console.error);
+                        }}>transferOwnership</button>
+                        <InputAddress placeholder='newOwner' state={newOwner} setState={setNewOwner}/>
+                    </p>
+                    <p>
+                        <button className="border-1 rounded-lg w-[200px] bg-companyL p-1 ml-0 m-2" onClick={() => {
+                            rootRouter?.renounceOwnershipOfCustomerNumber(BigNumber.from(selectedCode))
+                                .catch(console.error);
+                        }}>renounceOwnership</button>
+                    </p>
+                    <p>
+                        <button className="border-1 rounded-lg w-[200px] bg-companyL p-1 ml-0 m-2" onClick={() => {
+                            rootRouter?.setCustomerNumberSipDomain(BigNumber.from(selectedCode), newSipDomain)
+                                .catch(console.error);
+                        }}>setSipDomain</button>
+                        <InputString placeholder='newSipDomain' state={newSipDomain} setState={setNewSipDomain}/>
+                    </p>
+                    <p>
+                        <button className="border-1 rounded-lg w-[200px] bg-companyL p-1 ml-0 m-2" onClick={() => {
+                            rootRouter?.clearCustomerNumberSipDomain(BigNumber.from(selectedCode))
+                                .catch(console.error);
+                        }}>clearSipDomain</button>
+                    </p>
+                </div>
+            </>
+        );
+    }
+
+    function PoolSettings() {
+        const [newOwner, setNewOwner] = useState('');
+        const [newChainId, setNewChainId] = useState('');
+        const [newAddress, setNewAddress] = useState('');
+        const [newPoolCodeLength, setNewPoolCodeLength] = useState('');
+
+        return (
+            <>
+                <div>
+                    <p>Info:</p>
+                    <p>mode: {codeInfo.mode == 0 ? 'Number' : 'Pool'}</p>
+                    <p>router: {codeInfo.hasRouter ? null : '<MISSING>'}</p>
+                    {codeInfo.hasRouter ? <p>&nbsp;&nbsp;&nbsp;&nbsp;chainId: {codeInfo.router.chainId}</p> : null}
+                    {codeInfo.hasRouter ? <p>&nbsp;&nbsp;&nbsp;&nbsp;address: {codeInfo.router.adr}</p> : null}
+                    {codeInfo.hasRouter ? <p>&nbsp;&nbsp;&nbsp;&nbsp;poolCodeLength: {codeInfo.router.poolCodeLength}</p> : null}
+                </div>
+                <div className='pt-10'>
+                    <p>Methods:</p>
+                    <p>
+                        <button className="border-1 rounded-lg w-[200px] bg-companyL p-1 ml-0 m-2" onClick={() => {
+                            rootRouter?.changeCustomerNumberMode(BigNumber.from(selectedCode))
+                                .catch(console.error);
+                        }}>changeMode</button>
+                    </p>
+                    <p>
+                        <button className="border-1 rounded-lg w-[200px] bg-companyL p-1 ml-0 m-2" onClick={() => {
+                            rootRouter?.transferOwnershipOfCustomerNumber(BigNumber.from(selectedCode), newOwner)
+                                .catch(console.error);
+                        }}>transferOwnership</button>
+                        <InputAddress placeholder='newOwner' state={newOwner} setState={setNewOwner}/>
+                    </p>
+                    <p>
+                        <button className="border-1 rounded-lg w-[200px] bg-companyL p-1 ml-0 m-2" onClick={() => {
+                            rootRouter?.renounceOwnershipOfCustomerNumber(BigNumber.from(selectedCode))
+                                .catch(console.error);
+                        }}>renounceOwnership</button>
+                    </p>
+                    <p>
+                        <button className="border-1 rounded-lg w-[200px] bg-companyL p-1 ml-0 m-2" onClick={() => {
+                            rootRouter?.setCustomerNumberRouter(BigNumber.from(selectedCode), BigNumber.from(newChainId), newAddress, BigNumber.from(newPoolCodeLength))
+                                .catch(console.error);
+                        }}>setRouter</button>
+                        <InputNumber placeholder='newChainId' state={newChainId} setState={setNewChainId}/>
+                        <InputAddress placeholder='newAddress' state={newAddress} setState={setNewAddress}/>
+                        <InputNumber placeholder='newPoolCodeLength' state={newPoolCodeLength} setState={setNewPoolCodeLength}/>
+                    </p>
+                    <p>
+                        <button className="border-1 rounded-lg w-[200px] bg-companyL p-1 ml-0 m-2" onClick={() => {
+                            rootRouter?.clearCustomerNumberRouter(BigNumber.from(selectedCode))
+                                .catch(console.error);
+                        }}>clearRouter</button>
+                    </p>
+                </div>
+            </>
+        );
+    }
+
 
     if (!isLoaded) {
         return <Loader/>
@@ -76,17 +221,9 @@ export default function NumberManagementPage() {
                 {
                     selectedCode === -1
                         ? null
-                        : !isLoadedCodeInfo ? <Loader/> : (codeMode == 0 ? <NumberSettings/> : <PoolSettings/>)
+                        : !isLoadedCodeInfo ? <Loader/> : (codeInfo.mode == 0 ? <NumberSettings/> : <PoolSettings/>)
                 }
             </>
         );
     }
-}
-
-function NumberSettings() {
-    return <div></div>
-}
-
-function PoolSettings() {
-    return <div></div>
 }

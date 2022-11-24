dd711b33defbe1105871845b8390c8231b8e57b3
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-11-24 19:22:47 +0700

    fix swap window

--- src/pages/SwapPage.tsx
+++ src/pages/SwapPage.tsx
@@ -25,7 +25,7 @@ export default function SwapPage() {
     return (
         <div className='bg-white dark:bg-white mx-0 md:mx-[30px] flex items-center justify-center'>
             {/* <div className="mx-[30px] md:mx-0 p-[10px] max-h-[370px] max-w-[600px] bg-white dark:bg-white rounded-lg shadow-lg shadow-gray-400/30"> */}
-                <div className="flex flex-col items-center justify-center">
+                <div className="flex flex-col items-center justify-center shadow-lg shadow-gray-400/30 border-[1px]">
                     <SwapWidget width="100%" provider={provider} theme={theme}/>
                 </div>
             {/* </div> */}

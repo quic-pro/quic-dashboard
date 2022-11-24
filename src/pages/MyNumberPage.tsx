d7fe7332ea351789d447e56752601d6a18c4321f
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-11-24 19:11:37 +0700

    fix my number page

--- src/pages/MyNumberPage.tsx
+++ src/pages/MyNumberPage.tsx
@@ -66,8 +66,8 @@ export default function MyNumberPage() {
     } else {
         return (
             <div className='mx-0 md:mx-[30px] flex flex-row justify-center'>
-                <div className='bg-companyL dark:bg-companyD w-[600px] rounded-lg'>
-                    <div className='m-[10px] p-[10px] bg-white rounded-lg'>
+                <div className='bg-companyL dark:bg-companyD w-[600px] rounded-sm shadow-lg shadow-gray-400/30 border-[1px]'>
+                    <div className='m-[10px] p-[10px] bg-white rounded-sm'>
                         <div className='text-2xl font-medium text-companyL-400 dark:text-companyD-400'>
                             My numbers
                         </div>
@@ -75,15 +75,16 @@ export default function MyNumberPage() {
                             {myNumbers.length === 0 ? 'You don\'t have numbers' : null}
                             {myNumbers.map((code, index) => {
                                 if (code) {
-                                    return <button className="border-1 rounded-lg w-[70px] h-[40px] text-companyL-400 dark:text-companyD-400 bg-companyL dark:bg-companyD hover:bg-companyL-200 dark:hover:bg-companyD-200 border-[1px]"
-                                        key={index} onClick={() => selectNumber(index)}>{index}</button>
+                                    return <button className={"border-1 rounded-lg w-[70px] h-[40px] text-companyL-400 dark:text-companyD-400  hover:bg-companyL-200 dark:hover:bg-companyD-200 border-[1px]" +
+                                    (selectedCode===index?'bg-companyL-200 dark:bg-companyD-200':'bg-companyL dark:bg-companyD')}
+                                    key={index} onClick={() => {selectNumber(index); setSelectedCode(index)}}>{index}</button>
                                 } else {
                                     return null;
                                 }
                             })}
                         </div>
                         <div className='text-xl font-medium text-companyL-400 dark:text-companyD-400 py-[10px]'>
-                            Status of Number:
+                            {'Status of Number ' + selectedCode + ':'}
                         </div>
                         {
                             selectedCode === -1 ? null : (
@@ -92,8 +93,8 @@ export default function MyNumberPage() {
                                         <div>Mode: {codeMode == 0 ? 'Number' : 'Pool'}</div>
                                         {/* subscriptionEndTime */}
                                         <div>End in: {(new Date(codeStatus.subscriptionEndTime * 1000)).toUTCString()}</div>
-                                        <div>Hold status: {codeStatus.isHolded ? 'Holded' : 'Active'}</div>
-                                        <div>Lock status: {codeStatus.isBlocked ? 'Blocked' : 'Active'}</div>
+                                        <div>Hold status: <span className={(codeStatus.isHolded ? 'text-red-600' : 'text-green-600')}>{codeStatus.isHolded ? 'Holded' : 'Not hold'}</span></div>
+                                        <div>Lock status: <span className={(codeStatus.isBlocked ? 'text-red-600' : 'text-green-600')}>{codeStatus.isBlocked ? 'Blocked' : 'Not Block'}</span></div>
                                         {codeStatus.isHolded ? <div>subscriptionEndTime: {(new Date(codeStatus.holdingEndTime * 1000)).toUTCString()}</div> : null}
                                         <button className='border-1 rounded-lg p-1 m-4 ml-0 w-[200px] text-companyL-400 dark:text-companyD-400 bg-companyL dark:bg-companyD hover:bg-companyL-200 dark:hover:bg-companyD-200 border-[1px]' onClick={() => onRenewSubscription()}>Renew subscription</button>
                                     </div>

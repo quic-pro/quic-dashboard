d5031c7a5b667de2c54d12c45ed589069c492692
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-11-29 12:24:43 +0700

    recolor icons

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -219,11 +219,11 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                                                     <div className="flex items-center">
                                                         <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
                                                             <path
-                                                                className={`fill-current text-slate-600 ${pathname.includes('community') && 'text-indigo-500'}`}
+                                                                className={`fill-current text-companyL-300 ${pathname.includes('community') && 'text-indigo-500'}`}
                                                                 d="M18.974 8H22a2 2 0 012 2v6h-2v5a1 1 0 01-1 1h-2a1 1 0 01-1-1v-5h-2v-6a2 2 0 012-2h.974zM20 7a2 2 0 11-.001-3.999A2 2 0 0120 7zM2.974 8H6a2 2 0 012 2v6H6v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5H0v-6a2 2 0 012-2h.974zM4 7a2 2 0 11-.001-3.999A2 2 0 014 7z"
                                                             />
                                                             <path
-                                                                className={`fill-current text-slate-400 ${pathname.includes('community') && 'text-indigo-300'}`}
+                                                                className={`fill-current text-companyL-400 ${pathname.includes('community') && 'text-indigo-300'}`}
                                                                 d="M12 6a3 3 0 110-6 3 3 0 010 6zm2 18h-4a1 1 0 01-1-1v-6H6v-6a3 3 0 013-3h6a3 3 0 013 3v6h-3v6a1 1 0 01-1 1z"
                                                             />
                                                         </svg>
@@ -304,15 +304,15 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                                                     <div className="flex items-center">
                                                         <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
                                                             <path
-                                                                className={`fill-current text-slate-400 ${pathname.includes('ecommerce') && 'text-indigo-300'}`}
+                                                                className={`fill-current text-companyL-300 ${pathname.includes('ecommerce') && 'text-indigo-300'}`}
                                                                 d="M13 15l11-7L11.504.136a1 1 0 00-1.019.007L0 7l13 8z"
                                                             />
                                                             <path
-                                                                className={`fill-current text-slate-700 ${pathname.includes('ecommerce') && '!text-indigo-600'}`}
+                                                                className={`fill-current text-companyL-500 ${pathname.includes('ecommerce') && '!text-indigo-600'}`}
                                                                 d="M13 15L0 7v9c0 .355.189.685.496.864L13 24v-9z"
                                                             />
                                                             <path
-                                                                className={`fill-current text-slate-600 ${pathname.includes('ecommerce') && 'text-indigo-500'}`}
+                                                                className={`fill-current text-companyL-400 ${pathname.includes('ecommerce') && 'text-indigo-500'}`}
                                                                 d="M13 15.047V24l10.573-7.181A.999.999 0 0024 16V8l-11 7.047z"
                                                             />
                                                         </svg>
@@ -367,15 +367,15 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                                                     <div className="flex items-center">
                                                         <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
                                                             <path
-                                                                className={`fill-current text-slate-400 ${pathname.includes('finance') && 'text-indigo-300'}`}
+                                                                className={`fill-current text-companyL-300 ${pathname.includes('finance') && 'text-indigo-300'}`}
                                                                 d="M13 6.068a6.035 6.035 0 0 1 4.932 4.933H24c-.486-5.846-5.154-10.515-11-11v6.067Z"
                                                             />
                                                             <path
-                                                                className={`fill-current text-slate-700 ${pathname.includes('finance') && '!text-indigo-500'}`}
+                                                                className={`fill-current text-companyL-500 ${pathname.includes('finance') && '!text-indigo-500'}`}
                                                                 d="M18.007 13c-.474 2.833-2.919 5-5.864 5a5.888 5.888 0 0 1-3.694-1.304L4 20.731C6.131 22.752 8.992 24 12.143 24c6.232 0 11.35-4.851 11.857-11h-5.993Z"
                                                             />
                                                             <path
-                                                                className={`fill-current text-slate-600 ${pathname.includes('finance') && 'text-indigo-600'}`}
+                                                                className={`fill-current text-companyL-400 ${pathname.includes('finance') && 'text-indigo-600'}`}
                                                                 d="M6.939 15.007A5.861 5.861 0 0 1 6 11.829c0-2.937 2.167-5.376 5-5.85V0C4.85.507 0 5.614 0 11.83c0 2.695.922 5.174 2.456 7.17l4.483-3.993Z"
                                                             />
                                                         </svg>
@@ -447,11 +447,11 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                                         <div className="grow flex items-center">
                                             <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
                                                 <path
-                                                    className={`fill-current text-slate-600 ${pathname.includes('messages') && 'text-indigo-500'}`}
+                                                    className={`fill-current text-companyL-300 ${pathname.includes('messages') && 'text-indigo-500'}`}
                                                     d="M14.5 7c4.695 0 8.5 3.184 8.5 7.111 0 1.597-.638 3.067-1.7 4.253V23l-4.108-2.148a10 10 0 01-2.692.37c-4.695 0-8.5-3.184-8.5-7.11C6 10.183 9.805 7 14.5 7z"
                                                 />
                                                 <path
-                                                    className={`fill-current text-slate-400 ${pathname.includes('messages') && 'text-indigo-300'}`}
+                                                    className={`fill-current text-companyL-400 ${pathname.includes('messages') && 'text-indigo-300'}`}
                                                     d="M11 1C5.477 1 1 4.582 1 9c0 1.797.75 3.45 2 4.785V19l4.833-2.416C8.829 16.85 9.892 17 11 17c5.523 0 10-3.582 10-8s-4.477-8-10-8z"
                                                 />
                                             </svg>
@@ -484,19 +484,19 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                                                     <div className="flex items-center">
                                                         <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
                                                             <path
-                                                                className={`fill-current text-slate-600 ${pathname.includes('settings') && 'text-indigo-500'}`}
+                                                                className={`fill-current text-companyL-400 ${pathname.includes('settings') && 'text-indigo-500'}`}
                                                                 d="M19.714 14.7l-7.007 7.007-1.414-1.414 7.007-7.007c-.195-.4-.298-.84-.3-1.286a3 3 0 113 3 2.969 2.969 0 01-1.286-.3z"
                                                             />
                                                             <path
-                                                                className={`fill-current text-slate-400 ${pathname.includes('settings') && 'text-indigo-300'}`}
+                                                                className={`fill-current text-companyL-300 ${pathname.includes('settings') && 'text-indigo-300'}`}
                                                                 d="M10.714 18.3c.4-.195.84-.298 1.286-.3a3 3 0 11-3 3c.002-.446.105-.885.3-1.286l-6.007-6.007 1.414-1.414 6.007 6.007z"
                                                             />
                                                             <path
-                                                                className={`fill-current text-slate-600 ${pathname.includes('settings') && 'text-indigo-500'}`}
+                                                                className={`fill-current text-companyL-400 ${pathname.includes('settings') && 'text-indigo-500'}`}
                                                                 d="M5.7 10.714c.195.4.298.84.3 1.286a3 3 0 11-3-3c.446.002.885.105 1.286.3l7.007-7.007 1.414 1.414L5.7 10.714z"
                                                             />
                                                             <path
-                                                                className={`fill-current text-slate-400 ${pathname.includes('settings') && 'text-indigo-300'}`}
+                                                                className={`fill-current text-companyL-300 ${pathname.includes('settings') && 'text-indigo-300'}`}
                                                                 d="M19.707 9.292a3.012 3.012 0 00-1.415 1.415L13.286 5.7c-.4.195-.84.298-1.286.3a3 3 0 113-3 2.969 2.969 0 01-.3 1.286l5.007 5.006z"
                                                             />
                                                         </svg>
@@ -569,8 +569,8 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                                                 <div className="flex items-center justify-between">
                                                     <div className="flex items-center">
                                                         <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
-                                                            <path className="fill-current text-slate-600" d="M8.07 16H10V8H8.07a8 8 0 110 8z" />
-                                                            <path className="fill-current text-slate-400" d="M15 12L8 6v5H0v2h8v5z" />
+                                                            <path className="fill-current text-companyL-400" d="M8.07 16H10V8H8.07a8 8 0 110 8z" />
+                                                            <path className="fill-current text-companyL-300" d="M15 12L8 6v5H0v2h8v5z" />
                                                         </svg>
                                                         <span className="text-sm font-medium ml-3 2xl:opacity-100 duration-200">
                                                             Authentication

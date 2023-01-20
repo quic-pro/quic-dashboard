22d58acc79a7c605f1296dde5f30d218eac7af12
Sergey <50526865+Kosrotoff@users.noreply.github.com>
2023-01-21 01:21:42 +0700

    feat(chains): change the list of supported chains

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,56 +1,19 @@
 import {Chain} from 'wagmi';
-import {
-    arbitrum, arbitrumGoerli,
-    avalanche, avalancheFuji,
-    bsc, bscTestnet,
-    evmos, evmosTestnet,
-    fantom, fantomTestnet,
-    foundry,
-    gnosis, goerli,
-    hardhat,
-    iotex, iotexTestnet, localhost,
-    mainnet,
-    optimism, optimismGoerli,
-    polygon, polygonMumbai,
-    sepolia,
-    taraxa, taraxaTestnet,
-    zkSync, zkSyncTestnet,
-} from 'wagmi/chains';
+import {bsc, bscTestnet, mainnet, polygon, polygonMumbai, sepolia} from 'wagmi/chains';
 
 import {NODE_ENV} from './environment';
 
 
 const MAINNET_CHAINS: Readonly<Chain[]> = [
-    arbitrum,
-    avalanche,
-    bsc,
-    evmos,
-    fantom,
-    foundry,
-    gnosis,
-    iotex,
     mainnet,
-    optimism,
     polygon,
-    taraxa,
-    zkSync,
+    bsc,
 ];
 
 const TESTNET_CHAINS: Readonly<Chain[]> = [
-    arbitrumGoerli,
-    avalancheFuji,
-    bscTestnet,
-    evmosTestnet,
-    fantomTestnet,
-    goerli,
-    hardhat,
-    iotexTestnet,
-    localhost,
-    optimismGoerli,
-    polygonMumbai,
     sepolia,
-    taraxaTestnet,
-    zkSyncTestnet,
+    polygonMumbai,
+    bscTestnet,
 ];
 
 

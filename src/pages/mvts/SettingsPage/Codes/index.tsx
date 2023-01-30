d1e200393e5d662b4a50198fb21d6d593e55e91e
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2023-01-30 21:58:30 +0700

    feat (page account): change button style

--- src/pages/mvts/SettingsPage/Codes/index.tsx
+++ src/pages/mvts/SettingsPage/Codes/index.tsx
@@ -24,21 +24,26 @@ export default function Codes() {
 
     return (
         <div>
-            {accountCodes.data.map((code) => (
+            <div className="flex flex-wrap">
+                {accountCodes.data.map((code) => (
+                    <button
+                        key={code}
+                        onClick={() => handleCodeSelection(code)}
+                        className="border rounded-md p-1 m-1 w-[42px] h-[34px]
+                            bg-quicBlueL hover:bg-quicBlueL-200 text-quicBlueL-400
+                            dark:bg-quicBlueD dark:hover:bg-quicBlueD-200 dark:text-quicBlueD-400"
+                    >
+                        {code}
+                    </button>
+                ))}
                 <button
-                    key={code}
-                    onClick={() => handleCodeSelection(code)}
-                    className="border p-1 m-1"
+                    onClick={loadData}
+                    className="border rounded-md p-1 m-1 bg-quicBlueL hover:bg-quicBlueL-200 text-quicBlueL-400
+                        dark:bg-quicBlueD dark:hover:bg-quicBlueD-200 dark:text-quicBlueD-400"
                 >
-                    {code}
+                    Refresh
                 </button>
-            ))}
-            <button
-                onClick={loadData}
-                className="border p-1 m-1"
-            >
-                Refresh
-            </button>
+            </div>
             {selectedCode && <Control code={selectedCode}/>}
         </div>
     );

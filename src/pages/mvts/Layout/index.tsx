ced11610ec797a97f3d23d47430c497068a2485f
Sergey <50526865+Kosrotoff@users.noreply.github.com>
2023-02-13 00:07:33 +0700

    feat(mvts-layout): show a message during section preparation

--- src/pages/mvts/Layout/index.tsx
+++ src/pages/mvts/Layout/index.tsx
@@ -1,24 +1,38 @@
 import {getActualRootRouter} from '@mvts/resolver-js';
-import {useLayoutEffect} from 'react';
+import {useEffect, useLayoutEffect, useState} from 'react';
 import {Outlet} from 'react-router-dom';
 import {useSetRecoilState} from 'recoil';
 import {useNetwork, useSigner, useSwitchNetwork} from 'wagmi';
 
-import {rootRouterState} from '../../state/dashboard/mvts';
+import {rootRouterState} from '../../../state/dashboard/mvts';
+import MessageAboutSectionPreparation from './MessageAboutSectionPreparation';
 
 
 export default function Layout() {
+    const [isPrepared, setIsPrepared] = useState(false);
+
     const setRootRouter = useSetRecoilState(rootRouterState);
     const {data: signer} = useSigner();
     const {chain: currentChain} = useNetwork();
-    const {switchNetwork} = useSwitchNetwork();
+    const {switchNetwork} = useSwitchNetwork({
+        onSuccess: () => setIsPrepared(true),
+    });
 
     useLayoutEffect(() => {
+        if (isPrepared) {
+            setIsPrepared(false);
+        }
+    }, [currentChain]);
+
+    useEffect(() => {
         if (signer && currentChain && switchNetwork) {
             getActualRootRouter((chainId) => {
                 if (currentChain.id !== chainId) {
                     switchNetwork(chainId);
+                } else {
+                    setIsPrepared(true);
                 }
+
                 return signer;
             })
                 .then((rootRouter) => {
@@ -33,5 +47,5 @@ export default function Layout() {
         return () => setRootRouter(null);
     }, [signer, currentChain, switchNetwork]);
 
-    return <Outlet/>;
+    return isPrepared ? <Outlet/> : <MessageAboutSectionPreparation/>;
 }

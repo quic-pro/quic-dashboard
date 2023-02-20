15e6142b9618d39d5d3fa9491938c5fcc522c846
Sergey <50526865+Kosrotoff@users.noreply.github.com>
2023-02-20 12:54:24 +0700

    feat(connect-wallet): add information and link to documentation

--- src/features/web3/components/ConnectWallet/index.tsx
+++ src/features/web3/components/ConnectWallet/index.tsx
@@ -9,8 +9,10 @@ type Props = HTMLAttributes<HTMLDivElement>;
 export default function ConnectWallet({className = '', ...attributes}: Props) {
     return (
         <div {...attributes} className={'w-full flex flex-col ' + className}>
-            <span className="text-2xl mb-3 font-Comfort text-center">Connect wallet</span>
+            <span className="text-2xl font-Comfort text-center">Connect wallet</span>
+            <span className="mb-3 font-Comfort text-center italic">Choose a wallet to connect instead of creating passwords for every website.</span>
             <Connectors/>
+            <a href="https://ethereum.org/en/wallets/" className="mt-3 text-xs uppercase font-Comfort text-center text-quicBlueL-400 underline">I don&apos;t know what it is</a>
         </div>
     );
 }

b0bf3103a1223cabb9f3f76a4a279db428e3e93f
Sergey <50526865+Kosrotoff@users.noreply.github.com>
2023-02-21 02:38:18 +0700

    feat(settings): add notification close timeout setting

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,11 +1,37 @@
+import {ChangeEvent} from 'react';
+import {useRecoilState, useResetRecoilState} from 'recoil';
+import {popupNotificationCloseTimeoutState} from 'state/notifications';
+
 import SwitchTheme from './SwitchTheme';
+import {InputNumber} from './ui/inputs';
 
 
 export default function Settings() {
+    const [popupNotificationCloseTimeout, setPopupNotificationCloseTimeout] = useRecoilState(popupNotificationCloseTimeoutState);
+    const resetPopupNotificationCloseTimeout = useResetRecoilState(popupNotificationCloseTimeoutState);
+
+    const handleResetPopupNotificationCloseTimeout = resetPopupNotificationCloseTimeout;
+
+    const handleChangePopupNotificationCloseTimeout = (event: ChangeEvent<HTMLInputElement>) => {
+        setPopupNotificationCloseTimeout(Number(event.target.value));
+    };
+
     return (
-        <div className="flex flex-row p-2 bg-white border">
-            <div className="flex flex-row text-sm items-center">
+        <div className="flex flex-col p-2 bg-white border">
+            <div>
                 Change Theme: <SwitchTheme/>
+            </div>
+            <hr className="my-2"/>
+            <div>
+                <span>PopupNotificationCloseTimeout:</span>
+                <div className="flex">
+                    <InputNumber
+                        placeholder="PopupNotificationCloseTimeout"
+                        value={popupNotificationCloseTimeout}
+                        onChange={handleChangePopupNotificationCloseTimeout}
+                    />
+                    <button onClick={handleResetPopupNotificationCloseTimeout} className="ml-2 border rounded-md px-2">Reset</button>
+                </div>
             </div>
         </div>
     );
--- src/components/ui/inputs/Base.tsx
+++ src/components/ui/inputs/Base.tsx
@@ -2,5 +2,5 @@ import {BaseProps} from './types';
 
 
 export default function Base({className = '', ...attributes}: BaseProps) {
-    return <input {...attributes} className={'rounded-md h-8 px-2 ' + className}/>;
+    return <input {...attributes} className={'rounded-md border h-8 px-2 ' + className}/>;
 }

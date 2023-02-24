f048ec88a32c1e0ae5c59b18c4ba09f8f13959af
Sergey <50526865+Kosrotoff@users.noreply.github.com>
2023-02-24 21:55:35 +0700

    fix(mvts-layout): change the message about preparing the section

--- src/pages/dashboard/mvts/Layout/MessageAboutSectionPreparation.tsx
+++ src/pages/dashboard/mvts/Layout/MessageAboutSectionPreparation.tsx
@@ -2,8 +2,7 @@ export default function MessageAboutSectionPreparation() {
     return (
         <div className="flex-1 flex flex-col justify-center items-center">
             <div className="border border-red-500 text-red-500 p-4 text-2xl">
-                <span>To work with the MVTS project section, you need to switch to the chain in which the root router is located.</span>
-                <span>Please wait, data verification is in progress. You may need to change the chain.</span>
+                <span>Checking the current network...</span>
             </div>
         </div>
     );

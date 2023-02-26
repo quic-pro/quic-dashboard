355ce6f258fe204fe97de9fd773e5f1a679b4c5e
Sergey <50526865+Kosrotoff@users.noreply.github.com>
2023-02-26 17:26:46 +0700

    feat(layout-header): add menu

--- src/pages/Layout/Header/Navbar.tsx
+++ src/pages/Layout/Header/Navbar.tsx
@@ -1,13 +1,28 @@
 import {Link} from 'react-router-dom';
 
 
+const MENU = [
+    {
+        name: 'Home',
+        path: '/',
+    },
+    {
+        name: 'MVTS',
+        path: '/mvts-demo',
+        isNew: true,
+    },
+];
+
+
 export default function Navbar() {
     return (
         <nav className="flex flex-row items-center">
-            <Link to="/mvts-demo" className="flex flex-row px-2 py-1 border border-quicBlueL-300 rounded-xl">
-                MVTS
-                <span className="my-auto ml-1 px-1 border rounded-full bg-quicBlueD-300 text-xs">NEW</span>
-            </Link>
+            {MENU.map(({name, path, isNew}) => (
+                <Link key={name} to={path} className="flex flex-row mx-2 px-2 py-1 border border-quicBlueL-300 rounded-xl">
+                    {name}
+                    {isNew && <span className="my-auto ml-1 px-1 border rounded-full bg-quicBlueD-300 text-xs">NEW</span>}
+                </Link>
+            ))}
         </nav>
     );
 }

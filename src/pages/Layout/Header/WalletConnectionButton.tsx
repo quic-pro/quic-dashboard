7215dd7e312843ca7ea470db100c577469fd6788
Sergey <50526865+Kosrotoff@users.noreply.github.com>
2023-02-28 21:10:21 +0700

    feat(layout-header): change the text of the wallet connection button if already connected

--- src/pages/Layout/Header/WalletConnectionButton.tsx
+++ src/pages/Layout/Header/WalletConnectionButton.tsx
@@ -1,8 +1,13 @@
 import {Link} from 'react-router-dom';
+import {useAccount} from 'wagmi';
 
 
 export default function WalletConnectionButton() {
+    const {isConnected} = useAccount();
+
     return (
-        <Link to="/" className="border rounded-xl px-2 py-1 bg-quicBlueL-300 hover:shadow-lg hover:shadow-gray-400/30">Connect Wallet</Link>
+        <Link to="/" className="border rounded-xl px-2 py-1 bg-quicBlueL-300 hover:shadow-lg hover:shadow-gray-400/30">
+            {isConnected ? 'Change Wallet' : 'Connect Wallet'}
+        </Link>
     );
 }
